632d73e9fc61990ef46e5d7a222353483e345abd
kasamatsu <kasamatsu@kasa.com>
2023-05-10 14:02:42 +0900

    in.csv追加

--- ReadWS2812Signal/Program.cs
+++ ReadWS2812Signal/Program.cs
@@ -10,6 +10,24 @@ namespace ReadWS2812Signal
     {
         static void Main(string[] args)
         {
+            //元の、defineスイッチの設定を行う。
+            Init(false, false, true, true);
+
+            CodeStartSimple("pio0_sm0", 0, 1, 1, UNUSE, UNUSE, UNUSE, UNUSE, false);
+            WrapTarget();
+
+            Nop();
+            Nop();
+
+            Wrap();
+
+            CodeEnd(true, "pio0_sm0.pio");
+
+            RunEmulation(1200, "in.csv", "out.csv");
+
+            //そのままだとウィンドウが閉じてしまうので、入力待ちにしてウィンドウを閉じないようにする。
+            Console.Read();
+
         }
     }
 }
