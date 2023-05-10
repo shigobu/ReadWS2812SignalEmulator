dfea56c0b2fd7cacc465c3e818dafde682544ff9
kasamatsu <kasamatsu@kasa.com>
2023-05-10 17:40:43 +0900

    リセット検知追加

--- ReadWS2812Signal/Program.cs
+++ ReadWS2812Signal/Program.cs
@@ -10,20 +10,34 @@ namespace ReadWS2812Signal
     {
         static void Main(string[] args)
         {
+            const string readPin = "readPin";
+            const string resetCount = "resetCount";
+
             //元の、defineスイッチの設定を行う。
             Init(false, false, true, true);
             CodeStart("ReadWS2812", 0, 0, 1, UNUSE, UNUSE, UNUSE, UNUSE, false, false, true, 24, true, false, 0, 0, false, 1);
+
+            Pull(false, true, UNUSE, 0);
             WrapTarget();
 
-            Wait(true, Operands4.GPIO, 0, false, UNUSE, 9);
+            Wait(true, Operands4.GPIO, 0, false);
+            Label(readPin);
+            Mov(Operands1.X, Operands2.NONE, Operands1.OSR);
+            Nop(UNUSE, 7);
             In(Operands1.PINS, 1);
-            Wait(false, Operands4.GPIO, 0, false, UNUSE, 0);
+            Wait(false, Operands4.GPIO, 0, false);
+
+            Label(resetCount);
+            Jmp(Operands3.PIN, readPin);
+            Jmp(Operands3.X_NEQ_0_DEC, resetCount);
+
+            Irq(false, false, 0, false);
 
             Wrap();
 
             CodeEnd(true, "ReadWS2812.pio");
 
-            RunEmulation(1200, "in.csv", "out.csv");
+            RunEmulation(7000, "in.csv", "out.csv");
 
             //そのままだとウィンドウが閉じてしまうので、入力待ちにしてウィンドウを閉じないようにする。
             Console.Read();
